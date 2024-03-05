a794c2b191841f255f0c649c15929ce5b9382b25
Dhruv Solanki <95128593+dhruvsolanki0811@users.noreply.github.com>
2024-03-05 11:55:44 +0530

    Create a Navigation Bar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import logo from "@/assests/logo.svg";
+import Image from "next/image";
+import "@/components/Navbar/Navbar.css"
+function Navbar() {
+  return (
+    <>
+      <div className="nav w-full ps-10 pe-10 flex justify-between items-center ">
+        <div className="relative logo-container w-[4rem] h-[4rem] ">
+          <Image src={logo} alt="" fill></Image>
+        </div>
+        <div className="nav-items-container flex gap-4 font-bold text-[14px]">
+          <div className="nav-item flex items-center">Crypto Taxes</div>
+          <div className="nav-item flex items-center">Free Tools</div>
+          <div className="nav-item flex items-center">Resource Center</div>
+          <div className="nav-item flex items-center">
+            <div className="btn-container bg-[#2870ea] text-white p-[6px] rounded-[6px]">Get Started</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
